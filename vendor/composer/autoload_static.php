9e387a461c7f6e5a43ce0fc593e8512efeb1b21c
Sveta <sveta.budanaeva@gmail.com>
2019-07-02 15:51:18 +0300

    updates composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,7 +8,6 @@ class ComposerStaticInite7a955ab37b129a32d61b29c5c2545e2
 {
     public static $files = array (
         'be01b9b16925dcb22165c40b46681ac6' => __DIR__ . '/..' . '/wp-cli/php-cli-tools/lib/cli/cli.php',
-        '657df2afbb291ada14f4e9a5aaa5f88b' => __DIR__ . '/../..' . '/src/Games/Cli.php',
         'f8789535c80609b22e294a827b15dbcf' => __DIR__ . '/../..' . '/src/Games/Even.php',
         '9475cc7e877b484537e945d58b4e174a' => __DIR__ . '/../..' . '/src/Games/Calc.php',
         '1369f6f563d53c80394f909354e80d99' => __DIR__ . '/../..' . '/src/GameFunctions.php',
